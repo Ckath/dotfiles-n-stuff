e50195954b340dd8f30a6c9d68f65b5b4e834d8d
ckat <ckat@teknik.io>
2017-08-29 04:06:04 +0200

    make update output more readable

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@
 # shoddy way to update dotfiles to latest while keeping changes
 
 git stash
+printf "\n----------------------------------------------------- updating\n"
 git pull -r
+printf "\n----------------------------------------------------- restoring local changes\n"
 git stash pop
