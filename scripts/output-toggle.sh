ab2022a21df7af787d76a519eef81ba9c9403516
Ckat <ckat@teknik.io>
2017-11-29 23:40:44 +0100

    fix output toggle not toggling to original profile

--- scripts/output-toggle.sh
+++ scripts/output-toggle.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # toggle for hdmi/internal audio with notifications
 
-if cat /tmp/HDMI_TOGGLE 2> /dev/null; then
+if cat /tmp/PULSE_OLD_PROFILE 2> /dev/null; then
+    pacmd set-card-profile 0 output:"$(cat /tmp/PULSE_OLD_PROFILE)"
+    notify-send.sh -r 5 "Output toggle" "DEFAULT" -t 5
+    rm /tmp/PULSE_OLD_PROFILE
+else
+    pactl list sinks | grep Name | cut -c37- > /tmp/PULSE_OLD_PROFILE
     pacmd set-card-profile 0 output:hdmi-stereo
     notify-send.sh -r 5 "Output toggle" "HDMI" -t 5
-    rm /tmp/HDMI_TOGGLE
-else
-    pacmd set-card-profile 0 output:analog-surround-40
-    notify-send.sh -r 5 "Output toggle" "INTERMAL" -t 5
-    touch /tmp/HDMI_TOGGLE
 fi
