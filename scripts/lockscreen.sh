a4bbe4cf46aa07374c3691b1728bc016becade89
Ckat <ckat@teknik.io>
2018-04-15 15:08:30 +0200

    ditch i3lock colors for i3lock

--- scripts/lockscreen.sh
+++ scripts/lockscreen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# config for i3lock colors, 
+# config for i3lock
 # with pixelated/distored background screenshot
 
 scrot /tmp/screen.jpg
@@ -7,14 +7,4 @@ jpegoptim --size=1% /tmp/screen.jpg
 convert +noise Impulse -scale 20% -scale 501% /tmp/screen.jpg /tmp/screen.jpg
 convert /tmp/screen.jpg /tmp/screen.png
 
-i3lock -i /tmp/screen.png \
---textcolor=000000ff \
---insidecolor=ffffff1c \
---ringcolor=ffffff3e \
---linecolor=ffffff00 \
---keyhlcolor=00000080 \
---ringvercolor=0000ffff \
---separatorcolor=22222260 \
---insidevercolor=ffffff1c \
---ringwrongcolor=ff0000ff \
---insidewrongcolor=ffffff1c
+i3lock -e -f -i /tmp/screen.png
