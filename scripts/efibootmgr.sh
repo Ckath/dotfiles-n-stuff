391e61ae13564ace45d735570ead8c1ffbfc41fa
ckath <ckat@teknik.io>
2018-08-13 02:45:58 +0200

    mount ssd with discard for continuous trim

--- scripts/efibootmgr.sh
+++ scripts/efibootmgr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-efibootmgr -d /dev/sda -p 2 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/sda1 rw initrd=/initramfs-linux.img libahci.ignore_sss=1"
+efibootmgr -d /dev/sda -p 2 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/sda1 rootflags=discard rw initrd=/initramfs-linux.img libahci.ignore_sss=1"
