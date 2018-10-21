4b30a44e28cddbe3ca4e8137124631d63939da9d
ckath <ckat@teknik.io>
2018-10-21 16:31:12 +0200

    add desktop specific config

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -12,4 +12,6 @@ wmname LG3D
 xmodmap /home/cat/.xmodmap
 ~/.fehbg
 xbanish -bt5000
+xinput disable 9&
+xinput map-to-output 18 DVI-D-0&
 uim-xim&
--- scripts/efibootmgr.sh
+++ scripts/efibootmgr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-efibootmgr -d /dev/sda -p 2 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/sda1 rootflags=discard rw initrd=/initramfs-linux.img libahci.ignore_sss=1 i915.enable_rc6=1 i915.lvds_downclock=1 i915.semaphores=1"
+efibootmgr -d /dev/sdb -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/sdb2 rw initrd=/initramfs-linux.img libahci.ignore_sss=1"
