f2188195bc153db8f4411f0ee6aff9c8ebaca732
ckath <ckat@teknik.io>
2019-11-05 21:50:23 +0100

    use partuuid in efibootmgr

--- scripts/efibootmgr.sh
+++ scripts/efibootmgr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-efibootmgr -d /dev/sda -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/sda2 rw initrd=/initramfs-linux.img libahci.ignore_sss=1 libata.force=noncq sysrq_always_enabled=1"
+efibootmgr -d /dev/sda -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=PARTUUID=76d35af7-cd4a-c441-adc6-c263396d1b1f rw initrd=/initramfs-linux.img libahci.ignore_sss=1 libata.force=noncq sysrq_always_enabled=1"
