baa697871557ed9b063baf542d773d36a84db815
ckath <ckat@teknik.io>
2020-08-29 00:57:43 +0200

    might as well just use ffplay

--- scripts/capview.sh
+++ scripts/capview.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # small wrapper to have video+audio from capturecard
+
 LOADEDMOD=$(pactl load-module module-loopback source=alsa_input.usb-MACROSILICON_USB_Video-02.analog-stereo)
-mpv \
-		--profile=low-latency \
-		--untimed \
-		--demuxer-lavf-format=video4linux2 \
-		--demuxer-lavf-o-set=video_size=hd720 \
-		--demuxer-lavf-o-set=pixel_format=mjpeg \
-		/dev/video0
+ffplay -f video4linux2 -video_size hd720 -input_format mjpeg /dev/video0
 pactl unload-module $LOADEDMOD
