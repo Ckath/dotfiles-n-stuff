a62c1f892d2ae5acefcedf658253cb22ba6c55d1
ckath <ckat@teknik.io>
2020-12-03 15:35:35 +0100

    kinda update rec script but not really

--- scripts/rec.sh
+++ scripts/rec.sh
@@ -2,13 +2,11 @@
 # minimal region recording script with ffmpeg and slop
 
 # stop recording and convert to webm on second run
-if pgrep ffmpeg; then
-	pkill -9 ffmpeg;
-	ffmpeg -y -i /tmp/rec.mkv /tmp/rec.webm
-	exit 0;
-fi
+pgrep ffmpeg && pkill -9 ffmpeg && ffmpeg -y -i /tmp/rec.mkv /tmp/rec.webm
 
 # record selected region of screen
 rect=$(slop -f "%x %y %w %h %g %i") || exit 1
 read -r X Y W H G ID < <(echo $rect)
-ffmpeg -y -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y -f alsa -i pulse /tmp/rec.mkv
+# with audio
+# ffmpeg -y -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y -f alsa -i pulse /tmp/rec.mkv
+ffmpeg -y -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y /tmp/rec.mkv
