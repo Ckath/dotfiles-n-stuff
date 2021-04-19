4bc93692dc6ea148b557e603d81482b08420ad32
ckath <ckat@teknik.io>
2021-04-20 00:30:48 +0200

    fixup the mirror update script
    - more mirrors ranked
    - silence some outputs
    - single curl request

--- scripts/updatemirrors.sh
+++ scripts/updatemirrors.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # update mirrors from latest sources and pick the best ones
 
-get_mirrors() {
-	curl "https://archlinux.org/mirrorlist/?country=$1&protocol=https&ip_version=4&use_mirror_status=on" | sed 's/#S/S/g' >> /tmp/mirrors
-}
-
-get_mirrors "NL"
-get_mirrors "DE"
+echo "downloading mirrors..."
+curl -s "https://archlinux.org/mirrorlist/?country=NL&country=DE&protocol=https&ip_version=4&use_mirror_status=on" | sed 's/#S/S/g' >> /tmp/mirrors
 echo "ranking mirrors..."
-rankmirrors -n 8 /tmp/mirrors | sed '/##.*/d' | sudo tee /etc/pacman.d/mirrorlist
+rankmirrors -n 22 /tmp/mirrors | sed '/##.*/d' | sudo tee /etc/pacman.d/mirrorlist > /dev/null
+echo "done"
 rm /tmp/mirrors
