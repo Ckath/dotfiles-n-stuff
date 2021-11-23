ff3ab6dcdc7fcf7207d64494c12d25afbdc32334
ckath <ckat@teknik.io>
2021-11-23 21:34:35 +0100

    why did I separate the thinkpad stuff into a branch again

--- install.sh
+++ install.sh
@@ -22,42 +22,47 @@ ln -sfv "$DOT_DIR"/.vimrc \
 sudo ln -sfv "$DOT_DIR"/makepkg.conf /etc/makepkg.conf
 
 # server setup ends here, no need for this on servers
-if [ "$1" != "--server" ] && [ "$1" != "--vps" ]; then
-    rm -rf ~/scripts 2> /dev/null
-    ln -sfv "$DOT_DIR"/.Xresources \
-        "$DOT_DIR"/.xmodmap \
-        "$DOT_DIR"/.xinitrc \
-        "$DOT_DIR"/scripts ~/
-    mkdir ~/.weechat
-    ln -sfv "$DOT_DIR"/.weechat/* ~/.weechat
+[[ "$1" == "server" ]] || [[ "$1" == "vps" ]] && exit
 
-    mkdir -p ~/.config/ranger \
-        ~/.config/dunst \
-        ~/.config/pulseaudio-ctl \
-        ~/.config/networkmanager-dmenu \
-		~/.config/pulse 2> /dev/null
-    ln -sfv "$DOT_DIR"/.config/Trolltech.conf ~/.config
-    ln -sfv "$DOT_DIR"/.config/pulseaudio-ctl/config ~/.config/pulseaudio-ctl/
-    ln -sfv "$DOT_DIR"/.config/ranger/* ~/.config/ranger
-    ln -sfv "$DOT_DIR"/.config/pulse/* ~/.config/pulse
-    mkdir ~/.config/mpv 2> /dev/null || rm -rf ~/.config/mpv/scripts
-    ln -sfv "$DOT_DIR"/.config/mpv/* ~/.config/mpv
-	ln -sfv "$DOT_DIR"/.config/fuf ~/.config
+rm -rf ~/scripts 2> /dev/null
+ln -sfv "$DOT_DIR"/.Xresources \
+	"$DOT_DIR"/.xinitrc \
+	"$DOT_DIR"/scripts ~/
+mkdir ~/.weechat
+ln -sfv "$DOT_DIR"/.weechat/* ~/.weechat
 
-    sudo mkdir -p /etc/X11/xorg.conf.d
-    sudo ln -sfv "$DOT_DIR"/xorg.conf.d/* /etc/X11/xorg.conf.d
+mkdir -p ~/.config/ranger \
+	~/.config/dunst \
+	~/.config/pulseaudio-ctl \
+	~/.config/networkmanager-dmenu \
+	~/.config/pulse 2> /dev/null
+ln -sfv "$DOT_DIR"/.config/Trolltech.conf ~/.config
+ln -sfv "$DOT_DIR"/.config/pulseaudio-ctl/config ~/.config/pulseaudio-ctl/
+ln -sfv "$DOT_DIR"/.config/ranger/* ~/.config/ranger
+ln -sfv "$DOT_DIR"/.config/pulse/* ~/.config/pulse
+mkdir ~/.config/mpv 2> /dev/null || rm -rf ~/.config/mpv/scripts
+ln -sfv "$DOT_DIR"/.config/mpv/* ~/.config/mpv
+ln -sfv "$DOT_DIR"/.config/fuf ~/.config
 
-    sudo ln -sfv "$DOT_DIR"/pacman.conf \
-        "$DOT_DIR"/pakku.conf /etc
+sudo ln -sfv "$DOT_DIR"/pacman.conf \
+	"$DOT_DIR"/pakku.conf /etc
 
-	sudo mkdir -p /etc/X11/xorg.conf.d
+sudo ln -sfv "$DOT_DIR"/xorg.conf.d/* /etc/X11/xorg.conf.d
+
+$(cd ~/.mozilla/firefox/*.default && mkdir chrome)
+ln -svf "$DOT_DIR"/firefoxcss/* ~/.mozilla/firefox/*.default/chrome
+
+mkdir ~/.urxvt
+ln -sfv "$DOT_DIR"/.urxvt/* ~/.urxvt
+
+~/scripts/recolor.sh -f
+
+sudo mkdir -p /etc/X11/xorg.conf.d
+# thinkpad/desktop specifics
+if [[ $1 == "thinkpad"]]
+	sudo ln -sfv "$DOT_DIR"/thinkpad/xorg.conf.d/* /etc/X11/xorg.conf.d
+	sudo ln -sfv "$DOT_DIR"/thinkpad/xorg.conf.d/* /etc/X11/xorg.conf.d
+else
 	sudo ln -sfv "$DOT_DIR"/xorg.conf.d/* /etc/X11/xorg.conf.d
-
-    $(cd ~/.mozilla/firefox/*.default && mkdir chrome)
-    ln -svf "$DOT_DIR"/firefoxcss/* ~/.mozilla/firefox/*.default/chrome
-
-	mkdir ~/.urxvt
-	ln -sfv "$DOT_DIR"/.urxvt/* ~/.urxvt
-
-    ~/scripts/recolor.sh -f
+	ln -sfv "$DOT_DIR"/thinkpad/.xmodmap ~/
 fi
