f376956678858f7d85255e8ec0cc7aa2e89b931c
ckath <ckat@teknik.io>
2021-11-25 21:58:14 +0100

    update night.sh from thinkapd branch

--- scripts/night.sh
+++ scripts/night.sh
@@ -3,12 +3,12 @@
 
 if cat /tmp/BRIGHTNESS 2> /dev/null; then
     sct 0
-    xbacklight -set "$(cat /tmp/BRIGHTNESS)"
+    ybacklight -d intel_backlight -set "$(cat /tmp/BRIGHTNESS)"
     notify-send.sh -r 5 "Nightmode" "OFF" -t 5
     rm /tmp/BRIGHTNESS
 else
-    xbacklight -get > /tmp/BRIGHTNESS
+    ybacklight -d intel_backlight -get > /tmp/BRIGHTNESS
     sct 2200
-    xbacklight -set 1
+    ybacklight -d intel_backlight -set 0.7
     notify-send.sh -r 5 "Nightmode" "ON" -t 5
 fi
