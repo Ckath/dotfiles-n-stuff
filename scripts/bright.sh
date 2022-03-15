0fd7918e17f299633f82dc935224afbc89b3fb32
ckath <ckat@teknik.io>
2022-03-15 22:41:22 +0100

    move to ybacklight here too

--- scripts/bright.sh
+++ scripts/bright.sh
@@ -2,9 +2,9 @@
 # simple script to set brightness with notifications
 
 if [ "$1" = "+" ]; then
-    xbacklight + "$2" 
-    notify-send.sh -r 20 "Brightness up $2\nCurrent is $(xbacklight -get)"
+    ybacklight -d intel_backlight +"$2" 
+    notify-send.sh -r 20 "Brightness up $2\nCurrent is $(ybacklight -d intel_backlight -get)"
 elif [ "$1" = "-"  ]; then
-    xbacklight - "$2"
-    notify-send.sh -r 20 "Brightness down $2\nCurrent is $(xbacklight -get)"
+    ybacklight -d intel_backlight -"$2"
+    notify-send.sh -r 20 "Brightness down $2\nCurrent is $(ybacklight -d intel_backlight -get)"
 fi
