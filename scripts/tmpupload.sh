450fdc81b8268718426d19ad418f3bded30af4d9
ckath <ckath@yandex.ru>
2025-12-08 19:46:07 +0100

    fix files with spaces after actual years

--- scripts/tmpupload.sh
+++ scripts/tmpupload.sh
@@ -6,11 +6,14 @@ if [[ -z "$1" ]]; then
 	exit 1
 fi
 
+# prepare command with all files
 for f in "$@"; do # create flags
-	FILE_FLAGS="$FILE_FLAGS-F files[]=@\"$f\" "
+	CMD="$CMD -F 'files[]=@$f'"
 done
+CMD="curl $CMD https://xn--z7x.xn--6frz82g/tmp/api.php | xclip -selection clipboard"
+
 # upload and copy result to clipboard
-curl $FILE_FLAGS https://xn--z7x.xn--6frz82g/tmp/api.php | xclip -selection clipboard
+eval "$CMD"
 
 # notify about this happening (optional)
 xclip -o -selection clipboard | xargs -0 notify-send -i dialog-information
