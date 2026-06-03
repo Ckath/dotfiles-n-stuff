8e8631ab60031a2633359dda218ecb8337c4daa7
ckath <ckath@yandex.ru>
2026-06-03 19:25:01 +0200

    update tmpupload with new notify nonsense

--- scripts/tmpupload.sh
+++ scripts/tmpupload.sh
@@ -7,13 +7,12 @@ if [[ -z "$1" ]]; then
 fi
 
 # prepare command with all files
-for f in "$@"; do # create flags
-	CMD="$CMD -F 'files[]=@$f'"
-done
+for f in "$@"; do CMD="$CMD -F 'files[]=@$f'"; done
 CMD="curl $CMD https://xn--z7x.xn--6frz82g/tmp/api.php | xclip -selection clipboard"
 
 # upload and copy result to clipboard
 eval "$CMD"
 
 # notify about this happening (optional)
-xclip -o -selection clipboard | xargs -0 notify-send -i dialog-information
+command -v notifyck > /dev/null 2>&1 && \
+xclip -o -selection clipboard | notifyck -t "copied to clipboard"
